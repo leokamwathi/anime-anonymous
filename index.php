2ee0dd4ca3e21f11fe06c6a3599a4a787868294c
Leo Kamwathi <labwax@gmail.com>
2011-11-29 21:43:39 +0300

    FOR THEE THAT WILL BE,LET ALL THE POWER OF THEE A-STAR BE

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ class FusionTable {
 			//$str = trim( preg_replace( '/\s+/', ' ', $str ) );
 			//$str = $fields."\n".$str;
 			//$results = preg_split("/\n/", $str);
-			$results = preg_split("/\n/", $fields."\n".str_replace('\n',' ',substr(curl_exec ($c),strlen($fields))));
+			$results = preg_split("/\n/", $fields."\n".str_replace('\n',' ',trim(substr(curl_exec ($c),strlen($fields)))));
 			//$results = preg_split(",", $results);
 			//echo("<br/>It was me<br/><pre>");
 			//print_r($str);
