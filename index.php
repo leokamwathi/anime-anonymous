7828a90fe353d3786faeca62a46b2b03b516ae7e
Leo Kamwathi <labwax@gmail.com>
2011-12-03 00:28:23 +0300

    A-Star for life

--- index.php
+++ index.php
@@ -679,7 +679,8 @@ topList  += '</ul>';
 topList  += '</div>';
 	
 var fusiontabledata = "";
-fusiontabledata += topList+'<br/><div class="animeTitle" >'+title+'</div><div style="float: left;"><fb:like href="http://apps.facebook.com/anime-anonymous/?id='+mal_id +'" send="true" width="700" show_faces="false" font=""></fb:like></div>';
+//</div><div style="float: left;">
+fusiontabledata += topList+'<br/><div class="animeTitle" >'+title+'<fb:like href="http://apps.facebook.com/anime-anonymous/?id='+mal_id +'" send="true" width="700" show_faces="false" font=""></fb:like></div>';
 fusiontabledata += '<div><div><table border="0" width="100%" cellspacing="3" style="float: left;">';
 fusiontabledata += '<tr><td align="left" valign="top" colspan="2"><div id="leftbody"></div>';
 fusiontabledata += '</td></tr><tr><td width="210" align="left" valign="top"><table border="0" width="100%" cellspacing="3" cellpadding="3"><tr><td style="border-style: solid; border-width: 0px" bordercolor="#f7f7f7">';
