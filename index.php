22ee749c3cfc2cc632b6c5a063d8e27851e2f3ab
Leo Kamwathi <labwax@gmail.com>
2012-01-19 08:45:29 +0300

    something new

--- index.php
+++ index.php
@@ -1113,6 +1113,20 @@ String.prototype.br2nl = function() {
   /*height:100%;*/
 }
 </style>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3637395-7']);
+  _gaq.push(['_setDomainName', 'herokuapp.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body onload="getData();" style="background-color: #f7f7f7;" >
 
